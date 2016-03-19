06e915e4bd9a99ff32e3c4e2adea3d25e2a39217
Sasha Tilga <tilgaaleksandr@meta.ua>
2016-03-19 23:45:07 +0200

    old

--- js/app.js
+++ js/app.js
@@ -7,6 +7,50 @@ var meals =[
     "description": "description # 3"}
 ];
 
+var Store = {};
+
+Store.setList = function(){
+		if( !localStorage['list'] ) {
+			localStorage['list'] = JSON.stringify( [] );
+		};	
+		Store.list = JSON.parse ( localStorage['list'] );
+		return Store.list;
+};
+
+Store.pushToList = function (e) {
+	var item = {}, pass = 0;
+	item.name = e.target.dataset.name;
+	item.price = e.target.dataset.price;
+	item.description = e.target.dataset.description;
+	item.img = e.target.dataset.img;
+    
+    Store.setList();
+    if ( Store.list ) {
+    	for(var n in Store.list) {
+    		if (Store.list[n].name == item.name ) { pass++; }
+    		if (Store.list[n].price == item.price ) { pass++; }
+    		if (Store.list[n].description == item.description ) { pass++; }
+    		if (Store.list[n].img == item.img ) { pass++; }   			
+    		if ( pass == 4 ) {
+    			break;
+    		} else {
+    			pass = 0;
+    		}
+		}
+	}
+
+    if ( pass != 4 ) {
+    	Store.list.push( item );
+		localStorage['list'] = JSON.stringify( Store.list );
+	}
+
+
+	console.log( item );
+	console.log( Store.list );
+
+    e.preventDefault();
+};
+
 
 var Goods  = React.createClass({
 
@@ -23,8 +67,10 @@ var Goods  = React.createClass({
     				<p className="goods__item-body__description" > {item.description}</p> 
     				<input className="goods__item-body__add" 
     				 type='button' value='Add to List' 
-    				 name={item.name} price={item.price} 
-    				 description={item.description} img={item.img} />
+    				 data-name={item.name} data-price={item.price} 
+    				 data-description={item.description} data-img={item.img} 
+    				 onClick={Store.pushToList}
+    				 />
     			</div>
     			<div className='clearfix'></div>
     		</div>
@@ -45,12 +91,26 @@ var Goods  = React.createClass({
 
 
 var List  = React.createClass({
+
+  getInitialState: function() {
+    return {
+      list: Store.setList()
+    };
+  },
+
   render: function() {
+
+  	var body = !this.state.list.length ? 'No items are added to the list' :
+  	'You are awesome';
+
     return (
       <div className='list' >
             <div className="list-header">
         		Added to list
-
+                
+            </div>
+            <div className="list-body" >
+				{body}
             </div>	      		
       </div> 
     );
