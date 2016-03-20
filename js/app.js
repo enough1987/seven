5869590e67147d62c3017a581ef35a2e17c70ec9
Sasha Tilga <tilgaaleksandr@meta.ua>
2016-03-20 21:45:44 +0200

    add delete botton functionality

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 
 var meals =[
-    {"name":"one", "price":"99", "img" : "img/meals.jpg",
-    "description": "description # 1"}, 
-    {"name":"two", "price":"25", "img" : "img/crab-cakes.png", 
+    {"id" : "1", "name":"one", "price":"99", 
+    "img" : "img/meals.jpg", "description": "description # 1"}, 
+    {"id" : "2", "name":"two", "price":"25", "img" : "img/crab-cakes.png", 
     "description": "description # 2"}, 
-    {"name":"three","price":"48", "img" : "img/bacon_and_eggs.jpg", 
+    {"id" : "3", "name":"three","price":"48", "img" : "img/bacon_and_eggs.jpg", 
     "description": "description # 3"}
 ];
 
@@ -13,11 +13,15 @@ var meals =[
 
 var Goods  = React.createClass({
 
+	propTypes: {
+    	meals: React.PropTypes.array.isRequired
+    },
+
 	handleClick: function(e) {
 	
         if ( e.target.dataset.target == 'true' ) {
 			this.props.setList(e.target.dataset.name, e.target.dataset.price, 
-						e.target.dataset.img, e.target.dataset.description );
+						e.target.dataset.img, e.target.dataset.id );
 		}
 
 	},
@@ -36,7 +40,7 @@ var Goods  = React.createClass({
     				<input className="goods__item-body__add add_button" 
     				 type='button' value='Add to List' 
     				 data-name={item.name} data-price={item.price} 
-    				 data-description={item.description} data-img={item.img} 
+    				 data-id={item.id} data-img={item.img} 
     				 data-target='true'
     				 />
     			</div>
@@ -61,24 +65,44 @@ var Goods  = React.createClass({
 
 var List  = React.createClass({
 
+
+	handleClick: function(e) {
+	
+        if ( e.target.dataset.target == 'delete' ) {
+			this.props.deleteInList(e.target.dataset.index);
+		}
+
+	},
+
   render: function() {
 
 var body ='';
 if (this.props.list.length) {
       body = this.props.list.map(function(item, index) {
         return (
-          <div key={index}>
-            <p className="list-body__name">{item.name}:</p>
-            <p className="list-body__price">{item.price}</p>
-            <p className="list-body__img">{item.img}</p>
-            <p className="list-body__description">{item.description}</p>
-            <p className="list-body__ordered">{item.ordered}</p>            
-            <input type='button' value='add one more' />
+          <div key={index} className="list-body__item">
+            <img className="list-body__img" src={item.img} />
+            <div  className="list-body__text" >
+            	<p className="list-body__name">{item.name}</p>
+            	<p className="list-body__price">Price: 
+            	${item.price * item.ordered}</p>
+            	<p className="list-body__ordered">{item.ordered} item is ordered</p>            
+            	<input type='button' value='add one more' 
+            	className="list-body__button"
+            	data-index={index}  data-target='add'
+            	/> 
+            	<br />
+            	<input type='button' value='delete' 
+            	className="list-body__button"
+            	data-index={index} data-target='delete'
+            	/>
+            </div>
+            <div className='clearfix'> </div>
           </div>
         )
       })
     } else {
-      body = <p>'No items are added to the list'</p>
+      body = <p>No items are added to the list</p>
     }
 
     return (
@@ -87,14 +111,15 @@ if (this.props.list.length) {
         		Added to list
                 
             </div>
-            <div className="list-body" >
+            <div className="list-body"  onClick={this.handleClick}>
 				{body}
             </div>
+
             <div className={'list-body__buy ' + 
             (this.props.list.length ? '' : 'none' ) } >
 <p className="list-body__buy-length">
 List length is {this.props.list.length}</p>
-<input type='button' value='buy' />
+<input type='button' value='buy' className="list-body__buy-button" />
 	
             </div>
       </div> 
@@ -115,12 +140,12 @@ var App = React.createClass({
 
   	var me = this;
 
-	var setList = function(name, price, img, description) {
+	var setList = function(name, price, img, id) {
         var item = {};
         item.name = name;
         item.price = price;
         item.img = img;
-        item.description = description;
+        item.id = id;
         item.ordered = 1;
 
         me.state.list.forEach(function(ite, i, arr) {
@@ -128,7 +153,7 @@ var App = React.createClass({
         	if ( ite.name == item.name) { pass++; }
         	if ( ite.price == item.price) { pass++; }
         	if ( ite.img == item.img) { pass++; }
-        	if ( ite.description == item.description) { pass++; } 
+        	if ( ite.id == item.id) { pass++; } 
         	if ( pass == 4 ) { item.ordered = 0 }     	
         });
 
@@ -140,10 +165,19 @@ var App = React.createClass({
 
 	};
 
+	var deleteInList = function(index){
+
+		//delete me.state.list[index];
+		me.state.list.splice(index, 1);
+		me.setState( { list: me.state.list } );
+		console.log( me.state.list ); 
+		console.log( me.state.list.length ); 
+	};
+
     return (
       <div className="app">
         <Goods meals={meals} setList={setList} />
-        <List list={this.state.list} total={this.state.total} />
+        <List list={this.state.list} deleteInList={deleteInList} />
       </div>
     );
   }
